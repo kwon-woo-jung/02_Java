81ee8d149969d6e92f2942b240d0c02d0068e5b6
kwon-woo-jung <dnwnd1763@gmail.com>
2024-09-13 17:37:03 +0900

    추석 전 저장

--- 03_control/src/edu/kh/control/loop/WhileExample.java
+++ 03_control/src/edu/kh/control/loop/WhileExample.java
@@ -101,12 +101,20 @@ public class WhileExample {
 		 */
 		
 		
-		 Scanner sc = new Scanner(System.in);
+//		 Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		 
-		 System.out.println("정수 입력 : ");
+//		 System.out.println("정수 입력 : ");
+		 System.out.print("정수 입력 : ");
+		 
+//		 int input = sc.nextInt();
 		 int input = sc.nextInt();
 		
+//		 for(int row = 1; row <= input; row++) {
+//		 for(int row = 1; row <= input; row++) {
+		
 		 for(int row = 1; row <= input; row++) {
+	// 내가 입력한 input 값을 row에 대입하여 반복할때마다 1씩 증가
 			 
 			 for(int col = 1; col <=row; col++) {
 				 
@@ -288,12 +296,18 @@ public class WhileExample {
 		 		col이 1,2,3,4,5,6,7 일때 false 이므로 * 찍힘(공백찍힐일 없음)
 		* */
 		
+//		Scanner sc = new Scanner(System.in);
 		Scanner sc = new Scanner(System.in);
 		
+//		System.out.print("정수 입력 : ");
 		System.out.print("정수 입력 : ");
-		int input = sc.nextInt(); // 4일떄
+		
+//		int input = sc.nextInt(); // 4일떄
+		int input = sc.nextInt();
+		
 		
 		// 1부터 시작해서 input 까지 1씩 증가
+//		for(int row = 1; row <= input; row++) {
 		for(int row = 1; row <= input; row++) {
 			
 			for(int col = 1; col <= input + row -1; col++) {
@@ -301,13 +315,26 @@ public class WhileExample {
 				// [1-2턴] 	2 <= 4
 				// [1-3턴] 	3 <= 4
 				// [1-4턴] 	4 <= 4
+				// ---------------------------
+				// [2-1턴]  1 <= (4 + 2 -1) 5
+				// [2-2턴]  2 <= 5
+				// [2-3턴]  3 <= 5
+				// [2-4턴]  4 <= 5
+				// [2-5턴]  5 <= 5
 				
 				if(input-row >= col) { // 공백 조건
 			// [1-1턴] (4 - 1) 3 >= 1 (true) " " 출력
 			// [1-2턴]         3 >= 2 (true) " " 출력
-			// [1-2턴]         3 >= 3 (true) " " 출력
-			// [1-2턴]         3 >= 4 (false) "*" 출력
-					System.out.print(" ");
+			// [1-3턴]         3 >= 3 (true) " " 출력
+			// [1-4턴]         3 >= 4 (false) "*" 출력
+			// ---------------------------------
+			// [2-1턴] (4 - 2) 2 >= 1 (true) " " 출력
+			// [2-2턴] (4 - 2) 2 >= 2 (true) " " 출력
+			// [2-3턴] (4 - 2) 2 >= 3 (false) "*" 출력
+			// [2-4턴] (4 - 2) 2 >= 4 (false) "*" 출력
+			// [2-5턴] (4 - 2) 2 >= 5 (false) "*" 출력
+					
+						System.out.print(" ");
 					
 				} else { // * 조건
 					System.out.print("*");
@@ -315,7 +342,8 @@ public class WhileExample {
 				
 			}
 			
-			
+			System.out.println();
+			// [1-5] col이 5가 되면서 안쪽 for문 false로 줄바꿈하고 바깥for문 다시감
 			
 			
 		}
@@ -328,25 +356,108 @@ public class WhileExample {
 		
 		
 	}
+	
+	
+	
 	public void practice12() {
+		
+		/*다음과 같은 실행 예제를 구현하세요.
+
+		ex.
+		정수 입력 : 5
+		*****
+		* 	*
+		* 	*
+		* 	*
+		*****
+		
+		- input == row(행)
+		- 1행과 input행은 "*" 출력
+		- 1열과 input열은 "*" 출력
+		
+		
+		* */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for(int row = 1; row <= input; row++) {
+			
+			for(int col = 1; col < input; col++) {
+				
+				// 첫 번째 / 마지막 행, 첫번째 열, 마지막열 * 출력
+				if(row == 1 || row == input || col == 1 || col == input) {
+					System.out.print("*");
+					
+				} else { // 나머지는 공백 출력
+					System.out.print(" ");
+					
+				}
+				
+			}
+			
+			System.out.println();
+		}
+		
 		
 	}
 	
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public void practice13(){
+		
+		/*
+		 * 
+		 1부터 사용자에게 입력 받은 수까지 중에서
+		 1) 2와 3의 배수를 모두 출력하고
+		 2) 2와 3의 공배수의 개수를 출력하세요.
+		 
+		 
+		 ex)
+		 자연수 하나를 입력하세요 : 15
+		 2 3 4 6 8 9 10 12 14 15
+		 count : 2
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("자연수 하나를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		int count = 0;
+		
+		for(int i = 1; i <= input; i++) {
+			
+			// 2의 배수 또는 3의 배수인 경우 출력
+			if(i % 2 == 0 || i % 3 == 0) {
+				System.out.print(i + " ");
+				
+				// 2와 3의 공배수 == 2로도 나누어 떨어지고, 3으로도 나누어 떨어진다.
+				if(i % 2 == 0 && i % 3 == 0) {
+					// 갯수세기
+					count++;
+					
+				}
+				
+			}
+			
+		}
+		
+		System.out.println("\ncount : " + count);
+		
+		
+		
+		
+		
+		
 	
 	}
+	
+	
+}
 	
 	
 	
